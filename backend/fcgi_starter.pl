d3c513bc6d036ccc3316aad20f6a6d44eacbbdc4
Pavel Berezhnoy <pberezhnoy2005@gmail.com>
2014-10-24 01:43:45 +0400

    FCGI script configuration reimplemented

--- backend/fcgi_starter.pl
+++ backend/fcgi_starter.pl
@@ -3,18 +3,6 @@
 use strict;
 use warnings;
 
-use Sys::Syslog qw( :standard :macros );
-
-use CGI;
-use CGI::Util qw( unescape );
-use CGI::Fast;
-use FCGI::ProcManager qw( pm_manage pm_pre_dispatch pm_post_dispatch );
-use Digest::MD5 qw( md5_hex );
-
-use DBI;
-use POSIX;
-use JSON;
-
 package ErrHandler;
 
 use Tie::StdHandle;
@@ -39,29 +27,108 @@ sub WRITE {
 
 package main;
 
-my $server_name = "all_about";
-my %params = (
-    sock_path           => "/var/run/$server_name.sock",
-    port_used           => 0,
-    pid_path            => "/var/run/$server_name.pid",
-    need_root           => 1,
-    n_processes         => 1,
-    daemonize           => 0,
-    username            => 'nobody',
-    nginx_user          => 'http',
-    sql_user            => $server_name . '_user',
-    sql_pass            => $server_name . '_password',
-    sql_database_name   => $server_name,
-    sql_database_host   => 'localhost',
-    sql_database_port   => '3306',
-    log_level           => 5,
-    log_params          => { sql => 1, },
-    max_queries         => 3,
-);
+use Sys::Syslog qw( :standard :macros );
+use Getopt::Std;
+use Pod::Usage;
+
+use CGI;
+use CGI::Util qw( unescape );
+use CGI::Fast;
+use FCGI::ProcManager qw( pm_manage pm_pre_dispatch pm_post_dispatch );
+use Digest::MD5 qw( md5_hex );
+
+use DBI;
+use POSIX;
+use JSON;
+
+our $VERSION = '1.0';
+sub HELP_MESSAGE() { pod2usage(1) and exit 0; }
+sub VERSION_MESSAGE() { print STDERR "$0 v.$VERSION\n"; }
+
+my $server_name = "SimpleServer";
+my %global_parametrs;
+
+sub read_config {
+    my $cfg_path = shift;
+    my %params_types = (
+        server_name  => 'scalar',
+        sock_path    => 'scalar',
+        nginx_user   => 'scalar',
+        n_processes  => 'scalar',
+
+        db_name      => 'scalar',
+        db_user      => 'scalar',
+        db_pass      => 'scalar',
+        db_host      => 'scalar',
+        db_port      => 'scalar',
+
+        log_level    => 'scalar',
+        log_params   => 'lmap',
+    );
+
+    die "Can't open '$cfg_path': not found\n" unless -f $cfg_path;
+
+    open my $f, '<', $cfg_path;
+
+    while (<$f>) {
+        chomp;
+        next if $_ =~ /^\s*$/;
+
+        if (/^\s*([^\s]+)\s*=\s*(.*)/) {
+            my ($key, $val) = (lc($1), $2);
+            unless (defined $params_types{$key}) {
+                print STDERR "Unknown config file option found: '$key'\n";
+                next;
+            }
+
+            $val =~ s/\s*$//;
+            if ($key eq 'server_name') {
+                $server_name = $val;
+            } elsif ($params_types{$key} eq 'scalar') {
+                $global_parametrs{$key} = $val;
+            } elsif ($params_types{$key} eq 'list') {
+                $global_parametrs{$key} = [ split /[,\s]+/, $val ];
+            } elsif ($params_types{$key} eq 'lmap') {
+                $global_parametrs{$key} = { map { $_ => 1 } split /[,\s]+/, $val };
+            }
+        } else {
+            print STDERR "Unknown config file option line found: '$_'\n";
+        }
+    }
+
+    %global_parametrs = (
+        sock_path           => "/var/run/$server_name.sock",
+        pid_path            => "/var/run/$server_name.pid",
+        need_root           => 1,
+        n_processes         => 1,
+        daemonize           => 0,
+        username            => 'nobody',
+        nginx_user          => 'nobody',
+        db_user             => $server_name . '_user',
+        db_pass             => $server_name . '_password',
+        db_name             => $server_name,
+        db_host             => 'localhost',
+        db_port             => '3306',
+        log_level           => 1,
+        log_params          => {},
+        max_queries         => 30,
+        %global_paramets,
+    );
+}
+
+sub parse_cmd_line_args {
+    getopts("hc:d", \my %args);
+
+    pod2usage(1) and exit 0 if defined $args{h}; # Show help message and exit
+
+    read_config($args{c}) if defined $args{c};
+
+    $global_parametrs{daemonize} = defined $args{d}; # Daemonization
+}
 
 sub __log {
     my ($log_level, $type, $msg) = @_; # TODO: Show call point line number
-    syslog($type, $msg) if $log_level <= $params{log_level};
+    syslog($type, $msg) if $log_level <= $global_parametrs{log_level};
 }
 
 sub _log  { __log($_[0], LOG_INFO,      $_[1]); }
@@ -285,14 +352,14 @@ sub prepare_sth {
     my $sth_ref = $sql_queries{$query};
     my $sth = $sth_ref->{sth};
     unless ($sth) {
-        _log(1, "Preparing sql query '$query'") if $params{log_params}->{sql};
+        _log(1, "Preparing sql query '$query'") if $global_parametrs{log_params}->{sql};
         $sth = $dbh->prepare($query);
         $sth_ref->{sth} = $sth;
     }
 
     $sth_ref->{t} = localtime;
 
-    if (scalar(keys %sql_queries) > $params{max_queries}) {
+    if (scalar(keys %sql_queries) > $global_parametrs{max_queries}) {
         my $to_delete = undef;
         my $time = undef;
         for my $q (keys %sql_queries) {
@@ -308,7 +375,7 @@ sub prepare_sth {
 }
 
 sub check_user {
-    if ($params{need_root}) {
+    if ($global_parametrs{need_root}) {
         my $root_id = getpwnam 'root';
         my $user_id = getpwnam $ENV{USER};
         if ($root_id != $user_id) {
@@ -321,24 +388,23 @@ sub check_user {
 sub try_run {
     check_user;
 
-    if (-f $params{pid_path}) {
-        open my $pidfile, '<', $params{pid_path};
+    if (-f $global_parametrs{pid_path}) {
+        open my $pidfile, '<', $global_parametrs{pid_path};
         if ($pidfile) {
             my $pid = <$pidfile>;
             die "Already running with pid $pid\n" if kill 0, $pid;
         }
     }
-    open my $pidfile, '>', $params{pid_path};
+    open my $pidfile, '>', $global_parametrs{pid_path};
     print $pidfile $$;
-    unlink $params{sock_path} unless $params{port_used};
+    unlink $global_parametrs{sock_path};
 }
 
 sub get_uids {
-    my $user = shift || $params{username};
-    return (
-        uid => scalar getpwnam $user,
-        gid => scalar getgrnam $user
-    );
+    my $user = shift || $global_parametrs{username};
+    my $uid = scalar getpwnam $user or die "Can't get uid of user '$user': $!";
+    my $gid = scalar getgrnam $user or die "Can't get gid of user '$user': $!";
+    return ( uid => $uid, gid => $gid );
 }
 
 sub change_user {
@@ -360,13 +426,13 @@ sub reopen_std {
 }
 
 sub init {
-    daemonize if $params{daemonize};
+    daemonize if $global_parametrs{daemonize};
 
-    my %uids = get_uids $params{nginx_user};
-    my $socket = FCGI::OpenSocket($params{sock_path}, 10);
-    chown $uids{uid}, $uids{gid}, $params{sock_path} unless $params{port_used};
+    my %uids = get_uids $global_parametrs{nginx_user};
+    my $socket = FCGI::OpenSocket($global_parametrs{sock_path}, 10);
+    chown $uids{uid}, $uids{gid}, $global_parametrs{sock_path};
 
-    change_user if $params{daemonize};
+    change_user if $global_parametrs{daemonize};
 
     sub log_errors {
         my $msg = shift;
@@ -379,12 +445,12 @@ sub init {
 
     my $request = FCGI::Request(\*STDIN, \*STDOUT, \*STDERR, \%ENV, $socket, FCGI::FAIL_ACCEPT_ON_INTR)
         or die "Can't create request: $!\n";
-    pm_manage( n_processes => $params{n_processes} );
+    pm_manage( n_processes => $global_parametrs{n_processes} );
 
-    reopen_std if $params{daemonize};
+    reopen_std if $global_parametrs{daemonize};
 
-    my $dbh = DBI->connect("DBI:mysql:$params{sql_database_name}:" .
-        "$params{sql_database_host}:$params{sql_database_port}", $params{sql_user}, $params{sql_pass},
+    my $dbh = DBI->connect("DBI:mysql:$global_parametrs{db_name}:" .
+        "$global_parametrs{db_host}:$global_parametrs{db_port}", $global_parametrs{db_user}, $global_parametrs{db_pass},
         { RaiseError => 0, PrintError => 0, });
     die "Can't connect to DB: $!\n" unless $dbh;
 
@@ -429,5 +495,97 @@ sub init {
     FCGI::CloseSocket($socket);
 }
 
+parse_cmd_line_args;
 try_run;
 init;
+
+__END__
+
+=head1 NAME
+
+fcgi_starter.pl -- Simple FCGI server.
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-c>
+
+Set config file path. If not submitted, default config parametrs will be used.
+
+=item B<-d>
+
+Daemonize on start. All log messges will be printed in syslog.
+
+=item B<--help>
+
+Print a brief message and exit.
+
+=item B<--version>
+
+Print script version and exit.
+
+=back
+
+=head1 CONFIGURE FILE OPTIONS
+
+In this section all configure file options will be explained.
+If no information about a type of option value given, assume scalar (number or string) without quotes.
+
+=over 8
+
+=item B<server_name>
+
+Set FCGI server name. Default is SimpleServer.
+
+=item B<n_processes>
+
+Set FCGI process manager workers count. Default is 4.
+
+=item B<sock_path>
+
+Set FCGI socket path. Default is /var/run/_server_name_.sock.
+_server_name_ will be taken from B<server_name> option.
+
+=item B<nginx_user>
+
+Set nginx user name. For a socket permissions is needed [default: nobody].
+
+=item B<db_name>
+
+Set database name. Option is required.
+
+=item B<db_user>
+
+Set database user name. Option is required.
+
+=item B<db_pass>
+
+Set database user password. Option is required.
+
+=item B<db_host>
+
+Set database server hostname. localhost is default.
+
+=item B<db_port>
+
+Set database port number. 3306 is default.
+
+=item B<log_level>
+
+Set log level [1..5].
+All messages will be sent in syslog.
+
+=item B<log_params>
+
+A comma-splitted list of extra logging features.
+Available features:
+[ sql ]
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will start a simple FCGI server.
+
+=cut
